87a7feba5a33ec29592853064b77782582b59db9
ceppetto <pcy07111@gmail.com>
2021-07-05 16:55:10 +0900

    Solve 8958

---
+++ step_05/8958.py
@@ -0,0 +1,20 @@
+import sys
+n = int(input())
+resScore = [sys.stdin.readline() for _ in range(n)]
+
+sumScore = 0 # 총점을 위한 변
+cnt = 1 # O의 개수를 세기 위한 변수
+
+for i in range(n):
+    strValue = resScore[i]
+    for j in range(len(strValue)):
+        if 'O' == strValue[j]:
+            sumScore += cnt
+            cnt += 1
+        else:
+            cnt = 1
+    resScore[i] = str(sumScore)
+    sumScore = 0
+
+for i in range(n):
+    print(int(resScore[i]))
