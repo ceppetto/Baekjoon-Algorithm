43e53bd89a705db1cc37b74b69ac88b044a04efe
ceppetto <pcy07111@gmail.com>
2021-08-02 23:08:14 +0900

    Solve 1011

---
+++ step_08/1011.py
@@ -0,0 +1,23 @@
+import math
+T = int(input())
+
+for _ in range(T):
+    maxDistance = 1
+    count = 0
+
+    coordinates = list(map(int, input().split()))
+    x = coordinates[0]
+    y = coordinates[1]
+
+    distance = y - x
+
+    while distance > 0:
+        if distance >= maxDistance * 2:
+            count += 2
+            distance -= maxDistance * 2
+            maxDistance += 1
+        else:
+            count += 1
+            distance -= maxDistance
+
+    print(count)
