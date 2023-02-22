d20369de2280d989b9f1d92766483c49dc090d06
Chae-young Park <72439620+choala@users.noreply.github.com>
2023-02-22 22:24:46 +0900

    feat: Solve 10815 with 이진 탐색

---
+++ 이진 탐색/[실버 5] 10815 숫자 카드.py
@@ -0,0 +1,25 @@
+import sys
+
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return mid
+    elif array[mid] < target:
+        return binary_search(array, target, mid + 1, end)
+    else:
+        return binary_search(array, target, start, mid - 1)
+
+n = int(sys.stdin.readline().rstrip())
+num = list(map(int, sys.stdin.readline().rstrip().split()))
+num.sort()
+m = int(sys.stdin.readline().rstrip())
+x = list(map(int, sys.stdin.readline().rstrip().split()))
+
+for i in x:
+    res = binary_search(num, i, 0, n - 1)
+    if res == None:
+        print('0', end=' ')
+    else:
+        print(1, end=' ')
