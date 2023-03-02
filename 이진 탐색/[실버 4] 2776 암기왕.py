d11ade8e68a6552c754adb866227fb8a4d959575
Chae-young Park <72439620+choala@users.noreply.github.com>
2023-03-02 19:46:27 +0900

    feat: Solve 2776 with 이진 탐색

---
+++ 이진 탐색/[실버 4] 2776 암기왕.py
@@ -0,0 +1,28 @@
+import sys
+
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return mid
+    elif array[mid] < target:
+        return binary_search(array, target, mid + 1, end)
+    else:
+        return binary_search(array, target, start, mid - 1)
+
+t = int(sys.stdin.readline().rstrip())
+for _ in range(t):
+    n = int(sys.stdin.readline().rstrip())
+    num = list(map(int, sys.stdin.readline().rstrip().split()))
+    m = int(sys.stdin.readline().rstrip())
+    x = list(map(int, sys.stdin.readline().rstrip().split()))
+    
+    num.sort()
+    for i in x:
+        res = binary_search(num, i, 0, n - 1)
+        if res == None:
+            print('0')
+        else:
+            print('1')
+        
